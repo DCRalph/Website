59985f44a0493762104e9a23dadce0e3d0eb2b42
DCRalph <43304937+DCRalph@users.noreply.github.com>
2020-07-21 00:26:37 +1200

    m

--- script.js
+++ script.js
@@ -6,3 +6,51 @@ burger.addEventListener('click', e => {
   nav.dataset.state === "closed" ? nav.dataset.state = "open" : nav.dataset.state = "closed";
   nav.dataset.state === "open" ? nav.style.boxShadow = "0 0 15px #000" : nav.style.boxShadow = "0 0 0 #000";
 });
+
+const darkmode = document.querySelector('.switch input[type="checkbox"]');
+function switchTheme(e) {
+  if (e.target.checked){
+    document.documentElement.setAttribute('data-theme', 'dark');
+    setCookie("dark mode", "dark", 365);
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    setCookie("dark mode", "light", 365);
+    window.alert("Eww Gross. Light Mode");
+  }
+}
+darkmode.addEventListener('change', switchTheme, false);
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  var expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  var name = cname + "=";
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+  for(var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() {
+  var dark = getCookie("dark mode");
+  if (dark == "dark") {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    document.getElementById("dark-mode").checked = true;
+  } else if(dark == "light"){
+    document.documentElement.setAttribute('data-theme', 'light');
+    document.getElementById("dark-mode").checked = false;
+  }
+}
+checkCookie();
